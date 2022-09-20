4d395d3dbd18a82f2705ae16bbcf2b385d39ea0f
HelioAnacronista <heliojobs25@gmail.com>
2022-09-20 14:17:07 -0300

    start project

---
+++ src/main/java/io/github/helioanacronista/customerRegistrerFullCliente/CustomerRegistrerFullClienteApplication.java
@@ -0,0 +1,13 @@
+package io.github.helioanacronista.customerRegistrerFullCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrerFullClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegistrerFullClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/helioanacronista/customerRegistrerFullCliente/CustomerRegistrerFullClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.helioanacronista.customerRegistrerFullCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegistrerFullClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
